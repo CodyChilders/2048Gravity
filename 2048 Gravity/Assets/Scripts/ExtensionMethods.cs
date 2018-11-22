2d85e6b8d0ace228b6676d5be095d2a9769dab2e
Cody Childers <codyachilders@gmail.com>
2018-11-22 13:03:10 -0800

    Ball spawning
    They don't do anything yet, and no user input is accepted yet.

---
+++ 2048 Gravity/Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+static class ExtensionMethods
+{
+    public static UnityEngine.GameObject[] GetChildObjects(this UnityEngine.GameObject parent)
+    {
+        List<UnityEngine.GameObject> childSpawnLocations = new List<UnityEngine.GameObject>();
+        foreach (UnityEngine.Component childTransform in parent.GetComponentsInChildren(typeof(UnityEngine.Transform)))
+        {
+            if(childTransform.gameObject != parent)
+                childSpawnLocations.Add(childTransform.gameObject);
+        }
+        return childSpawnLocations.ToArray();
+    }
+}
---
+++ 2048 Gravity/Assets/Scripts/SpawnBalls.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBalls : MonoBehaviour
+{
+    public GameObject ballTemplate;
+    public float spawnFrequency = 1;
+
+    GameObject[] ballSpawns;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        ballSpawns = gameObject.GetChildObjects();
+        Invoke("SpawnBall", spawnFrequency);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnBall()
+    {
+        GameObject chosenSpawn = SelectSpawn();
+        GameObject newBall = Instantiate(ballTemplate);
+        newBall.transform.position = chosenSpawn.transform.position;
+
+        //chain this into the next ball spawning
+        Invoke("SpawnBall", spawnFrequency);
+    }
+
+    GameObject SelectSpawn()
+    {
+        Debug.Assert(ballSpawns != null);
+        Debug.Assert(ballSpawns.Length > 0);
+
+        float rand01 = Random.value;
+        float randRange = rand01 * ballSpawns.Length;
+        int index = Mathf.FloorToInt(randRange);
+        return ballSpawns[index];
+    }
+}

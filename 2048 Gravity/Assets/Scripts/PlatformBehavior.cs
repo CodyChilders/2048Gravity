e0a694fc7b7f109ca26b7c3b8c9b52a20aca5953
Cody Childers <codyachilders@gmail.com>
2018-11-23 18:05:40 -0800

    Removed obsolete code

--- 2048 Gravity/Assets/Scripts/PlatformBehavior.cs
+++ 2048 Gravity/Assets/Scripts/PlatformBehavior.cs
@@ -6,25 +6,14 @@ public class PlatformBehavior : MonoBehaviour
 {
     Quaternion initialOrientation;
 
-    // Use this for initialization
     void Start()
     {
         initialOrientation = transform.rotation;
     }
 
-    // Update is called once per frame
     void Update()
     {
         ChangeGravity();
-        /*
-        Vector3 acc = Input.acceleration.normalized;
-        
-        float xAngle = Vector3.Angle(acc, Vector3.down);
-        //float zAngle = Vector3.Angle(acc, Vector3.forward);
-
-        transform.rotation = initialOrientation;
-        transform.Rotate(Vector3.forward, xAngle);
-        //transform.Rotate(Vector3.forward, zAngle);*/
     }
 
     void ChangeGravity()
@@ -43,11 +32,4 @@ public class PlatformBehavior : MonoBehaviour
         ret.z *= -1;
         return ret;
     }
-
-    /*private void OnGUI()
-    {
-        Vector3 acc = Input.acceleration.normalized;
-        string accStr = string.Format("[{0}, {1}, {2}]", acc.x, acc.y, acc.z);
-        GUI.Label(new Rect(5, 5, 20, 20), accStr);
-    }*/
 }
